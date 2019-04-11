c3cf51c8d344ad92f2fc2ff5e6a7bc7ccc5b3c0b
Nina <nina.w@atelier-cartographique.com>
2019-04-11 10:28:01 +0200

    passage a la v2

--- www/js/image.js
+++ www/js/image.js
@@ -3,49 +3,49 @@
  *
  */
 
-(function generate_image_blocks () {
+(function generate_image_blocks() {
     'use strict';
 
     var IMG_URL_PREFIX = '/medias/images/';
     var IMG_URL_SUFFIX = '.jpg';
     var DATA_ATTRIBUTE = 'image';
 
-    if (window.location.protocol === 'file:' ) {
+    if (window.location.protocol === 'file:') {
         IMG_URL_PREFIX = '../medias/images/';
     }
 
-    function attribute () {
+    function attribute() {
         return 'data-' + DATA_ATTRIBUTE;
     }
 
-    function selector () {
+    function selector() {
         return '[' + attribute() + ']';
     }
 
-    function img_url (name) {
+    function img_url(name) {
         return ['url("', IMG_URL_PREFIX, name, IMG_URL_SUFFIX, '")'].join('');
     }
 
-    function css_rule (url) {
+    function css_rule(url) {
         return ['background-image:', url, ';'].join('');
     }
 
     var id_base = 0;
-    function id_gen () {
+    function id_gen() {
         id_base += 1;
         return 'img_' + id_base;
     }
 
-    function generate_block (elem) {
+    function generate_block(elem) {
         var id = id_gen();
         var url = img_url(elem.getAttribute('data-image'));
-        var css_decl = ['#', id, '{', css_rule(url) ,'}'].join('');
+        var css_decl = ['#', id, '{', css_rule(url), '}'].join('');
         elem.id = id;
 
         return css_decl;
     }
 
-    function generate_style () {
+    function generate_style() {
         var elementList = document.querySelectorAll(selector());
         var style = [];
         for (var i = 0; i < elementList.length; i++) {
@@ -55,10 +55,10 @@
         return style.join('\n');
     }
 
-    function write_style () {
+    function write_style() {
         var style_decl = document.createTextNode(generate_style());
         var style_elem = document.createElement('style');
-        var head = document.querySelector('header');
+        var head = document.querySelector('head');
         style_elem.type = 'text/css';
         style_elem.appendChild(style_decl);
         head.appendChild(style_elem);
---
+++ www/old/collection/gen.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+
+import sys
+import csv
+from pathlib import Path
+from slugify import slugify
+from string import Template
+
+
+def main():
+    db_path = Path(sys.argv[1])
+    tpl_path = Path(sys.argv[2])
+    out_dir = Path(sys.argv[3])
+    tpl_link_path = Path(sys.argv[4])
+    base_url = sys.argv[5]
+    if out_dir.exists() == False:
+        out_dir.mkdir(parents=True)
+
+    template = None
+    with open(tpl_path.as_posix()) as f:
+        template = Template(f.read())
+
+    template_link = None
+    with open(tpl_link_path.as_posix()) as f:
+        template_link = Template(f.read())
+
+    if template == None:
+        raise Exception('failed to build template')
+    if template_link == None:
+        raise Exception('failed to build template_link')
+
+    with open(db_path.as_posix()) as f:
+        r = csv.DictReader(f)
+
+        for row in r:
+            slug = slugify(row['PRENOM'] + '-' + row['NOM'])
+
+            paragraphs = '\n'.join(
+            ['<p>{}</p>'.format(p) for p in row['BIOGRAPHIE'].split('\n')])
+            row['BIOGRAPHIE'] = paragraphs
+
+            image = row['IMAGE'].split('.')[0]
+            row['IMAGE'] = image
+
+            content = template.substitute(row, slug=slug, base_url=base_url)
+            link = template_link.substitute(row, slug=slug, base_url=base_url)
+            dest_path = out_dir.joinpath('{}.html'.format(slug))
+            with open(dest_path.as_posix(), 'w') as dest:
+                dest.write(content)
+
+            print(link)
+
+
+if __name__ == '__main__':
+    main()
