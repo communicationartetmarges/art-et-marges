2a45e46d19bb9fadd241363eb7793e84c574bc84
nina <nina.w@atelier-cartographique.be>
2020-02-04 10:50:56 +0100

    issues #726 #727 #728 #729 #730 #731

--- www/js/image.js
+++ www/js/image.js
@@ -8,22 +8,24 @@
 
     var IMG_URL_PREFIX = '/medias/images/';
     var IMG_URL_SUFFIX = '.jpg';
-    var DATA_ATTRIBUTE = 'image';
+    var GIF_URL_SUFFIX = '.gif';
+    var IMG_ATTRIBUTE = 'image';
+    var GIF_ATTRIBUTE = 'gif';
 
     if (window.location.protocol === 'file:') {
         IMG_URL_PREFIX = '../medias/images/';
     }
 
-    function attribute() {
-        return 'data-' + DATA_ATTRIBUTE;
+    function selector(attr) {
+        return '[' + 'data-' + attr + ']';
     }
 
-    function selector() {
-        return '[' + attribute() + ']';
-    }
-
-    function img_url(name) {
-        return ['url("', IMG_URL_PREFIX, name, IMG_URL_SUFFIX, '")'].join('');
+    function img_url(name, attr) {
+        if (attr == IMG_ATTRIBUTE) {
+            return ['url("', IMG_URL_PREFIX, name, IMG_URL_SUFFIX, '")'].join('');
+        } else if (attr == GIF_ATTRIBUTE) {
+            return ['url("', IMG_URL_PREFIX, name, GIF_URL_SUFFIX, '")'].join('');
+        }
     }
 
     function css_rule(url) {
@@ -36,9 +38,10 @@
         return 'img_' + id_base;
     }
 
-    function generate_block(elem) {
+    function generate_block(elem, attr) {
         var id = id_gen();
-        var url = img_url(elem.getAttribute('data-image'));
+        var s = 'data-' + attr;
+        var url = img_url(elem.getAttribute(s), attr);
         var css_decl = ['#', id, '{', css_rule(url), '}'].join('');
         elem.id = id;
 
@@ -46,10 +49,15 @@
     }
 
     function generate_style() {
-        var elementList = document.querySelectorAll(selector());
+        var elementList = document.querySelectorAll(selector(IMG_ATTRIBUTE));
+        var attr = IMG_ATTRIBUTE;
+        if (elementList.length < 1) {
+            elementList = document.querySelectorAll(selector(GIF_ATTRIBUTE));
+            attr = GIF_ATTRIBUTE;
+        }
         var style = [];
         for (var i = 0; i < elementList.length; i++) {
-            style.push(generate_block(elementList[i]));
+            style.push(generate_block(elementList[i], attr));
         }
 
         return style.join('\n');
