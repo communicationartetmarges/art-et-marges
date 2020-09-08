c20daec334a8bd4c1d05e7f8e110d47a2b2eb6eb
Nina Willems <nina.w@atelier-cartographique.be>
2020-09-08 13:33:05 +0200

    issue #845

--- www/js/splash.js
+++ www/js/splash.js
@@ -3,21 +3,85 @@
         e.parentElement.removeChild(e)
     }
 
-    function init() {
+    function getLocaleStorage() {
+        try {
+            const storage = window.localStorage;
+            const x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return storage;
+        }
+        catch (e) {
+            return false;
+        }
+    };
+
+    const toLocalStorage =
+        (key, value) => {
+            const storage = getLocaleStorage();
+            if (storage) {
+                storage.setItem(key, JSON.stringify(value));
+            }
+        };
+
+    const getLocalStorageValue = (key, dflt) => {
+        const storage = getLocaleStorage();
+        if (storage) {
+            const jsonString = storage.getItem(key);
+            if (jsonString) {
+                return JSON.parse(jsonString);
+            }
+        }
+        return dflt;
+    };
+
+    function tryInit(f, name) {
+        try { f() } catch (err) {
+            console.log('Failed Init: ' + name)
+            console.log(err);
+        }
+    }
+
+    function initSpash() {
+        const splashState = getLocalStorageValue('splash', [])
+
+        document
+            .querySelectorAll(".splash")
+            .forEach((splash) => {
+                const id = splash.id;
+                if (splashState.indexOf(id) < 0) { // never seen
+                    toLocalStorage('splash', splashState.concat([id]));
+                    splash
+                        .querySelectorAll(".splash--close")
+                        .forEach((close) => {
+                            close.addEventListener("click", () => removeElem(splash))
+                        })
+                }
+                else {
+                    removeElem(splash)
+                }
+            })
+    }
+
+    function initVideo() {
         let videoPlay = document.getElementById("video-play");
         let videoPause = document.getElementById("video-pause");
 
         videoPlay.addEventListener('click', (evt) => {
             let myVideo = document.getElementById("video1");
             myVideo.play();
-            removeElem(videoPlay)
         })
         videoPause.addEventListener('click', () => {
             let myVideo = document.getElementById("video1");
-            let splash = document.getElementById("Splash");
-            removeElem(splash)
+            myVideo.pause();
         })
     }
+
+    function init() {
+        tryInit(initSpash, 'splash');
+        tryInit(initVideo, 'video');
+    }
+
 
     document.onreadystatechange = function startApplication() {
         if ('interactive' === document.readyState) {
@@ -25,3 +89,4 @@
         }
     };
 })()
+
